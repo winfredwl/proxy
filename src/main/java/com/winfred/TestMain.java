0cb034610cb8ce85e0568ac7f2bc1ad7e60cda9c
winfredwl <609592629@qq.com>
2017-05-06 15:07:40 +0800

    JDK动态代理

--- src/main/java/com/winfred/TestMain.java
+++ src/main/java/com/winfred/TestMain.java
@@ -1,15 +1,23 @@
 package com.winfred;
 
+import com.winfred.dynamicproxy.DynamicProxy;
 import com.winfred.staticproxy.Hello;
+import com.winfred.staticproxy.HelloImpl;
 import com.winfred.staticproxy.HelloProxy;
 
 /**
- * Created by Administrator on 2017/5/6 0006.
+ * @author Winfred.Wang
+ * @since 1.0.0
  */
 public class TestMain {
 
     public static void main(String[] args) {
-        Hello helloProxy = new HelloProxy();
+//        Hello helloProxy = new HelloProxy();
+//        helloProxy.say("Jack");
+
+        DynamicProxy dynamicProxy = new DynamicProxy(new HelloImpl());
+        Hello helloProxy = dynamicProxy.getProxy();
+
         helloProxy.say("Jack");
     }
 }
---
+++ src/main/java/com/winfred/dynamicproxy/DynamicProxy.java
@@ -0,0 +1,43 @@
+package com.winfred.dynamicproxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+/**
+ * @author Winfred.Wang
+ * @since 1.0.0
+ */
+public class DynamicProxy implements InvocationHandler{
+
+    private Object target;
+
+    public DynamicProxy(Object target) {
+        this.target = target;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        before();
+        Object result = method.invoke(target, args);
+        after();
+        return result;
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T getProxy() {
+        return (T) Proxy.newProxyInstance(
+                target.getClass().getClassLoader(),
+                target.getClass().getInterfaces(),
+                this
+        );
+    }
+
+    private void before() {
+        System.out.println("Before");
+    }
+
+    private void after() {
+        System.out.println("After");
+    }
+}
