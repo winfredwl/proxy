6aee5255f7d74af53b5e8d6c44dcf0613c4305c4
wanglei <wangl1038@joyowo.com>
2017-05-17 11:14:17 +0800

    Spring AOP:抛出增强

--- src/main/java/com/winfred/springaop/GreetingImpl.java
+++ src/main/java/com/winfred/springaop/GreetingImpl.java
@@ -12,5 +12,7 @@ public class GreetingImpl implements Greeting {
     @Override
     public void sayHello(String name) {
         System.out.println("Hello! " + name);
+
+        throw new RuntimeException("Error");
     }
 }
